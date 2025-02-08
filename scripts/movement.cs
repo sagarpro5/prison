8040f356f65fa3e6fcbe945b3efe50ca672b458e
sagarpro5 <sagarshewale4576@gmail.com>
2025-02-08 19:14:08 +0530

    code 1.1

---
+++ scripts/movement.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    Vector2 movedir;
+    float speed = 7f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.up * speed * Time.deltaTime;
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * speed * Time.deltaTime;
+
+        }
+    }
+}
